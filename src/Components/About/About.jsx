4b6af195dfcbe1ddf2be28f5831e610cdf5a9da1
sahil <darjisahil46@gmail.com>
2026-03-01 21:20:44 +0530

    update portfolio

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -51,7 +51,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1Zwo9DoTFRgz5yEa3Sx-bKyieJ82XNYdN/view?usp=sharing"
+            href="https://drive.google.com/file/d/1KQHF_vq6eJqU48qCt1S39q6YJaTRHLk-/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
